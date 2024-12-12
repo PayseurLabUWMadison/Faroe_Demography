0d149819130e7c9654bb2bab4874faca1bbb6b20
Jushudao <31456233+Jushudao@users.noreply.github.com>
2024-12-12 15:19:12 -0600

    Update mark_duplicates.sh

--- Reads to Variants/Scripts/mark_duplicates.sh
+++ Reads to Variants/Scripts/mark_duplicates.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+java -Xmx20g -jar /picard/build/libs/picard.jar MarkDuplicates \
+INPUT=/bamfile-directory/${sample}.bam \
+OUTPUT=/dedup-bamfile-directory/${sample}_dedup.bam \
+METRICS_FILE=/dedup-bamfile-directory/${sample}_duplicate.metrics \
+REMOVE_DUPLICATES=true \
+ASSUME_SORTED=true TMP_DIR=/tmp_${sample}/ \
+MAX_RECORDS_IN_RAM=500000 \
+VALIDATION_STRINGENCY=LENIENT &
