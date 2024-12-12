d36c4f07b9f1053603939747e156a8d0ddd899b8
Jushudao <31456233+Jushudao@users.noreply.github.com>
2024-12-12 15:09:07 -0600

    Update bwa_mem_alignment.sh

--- Reads to Variants/Scripts/bwa_mem_alignment.sh
+++ Reads to Variants/Scripts/bwa_mem_alignment.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+infilename=$(echo "$(basename "$1")" | head -n 1 | cut -f1 -d".")
+outfilename=$(echo $infilename | head -n 1 | cut -f 1-3 -d"_")
+# echo $outfilename
+sm=$(echo "$(basename "$1")" | head -n 1 | cut -f1 -d"_")
+header=$(zcat $1 | head -n 1)
+id=$(echo $header | head -n 1 | cut -f 1-3 -d":" | sed 's/@//' | sed 's/:/_/g')
+lane=$(echo $header | head -n 1 | cut -f4 -d":")
+# echo "Read Group @RG\tID:$id.$lane\tPU:$id.$lane.$sm\tSM:$sm\tLB:library"_"$sm\tPL:ILLUMINA"
+
+bwa-mem2-directory/bwa-mem2 mem -M -t 4 \
+-R $(echo "@RG\tID:$id.$lane\tPU:$id.$lane.$sm\tSM:$sm\tLB:library"_"$sm\tPL:ILLUMINA") \
+mm39-directory/mm39.fa \
+$1 $2 | samtools sort -O bam -o ${outfilename}_adapterTrimmed.bam
